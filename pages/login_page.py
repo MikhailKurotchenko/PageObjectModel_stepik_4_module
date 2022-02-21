3c670434b7671e9f7f7182f6156254591576f5d1
MikhailKurotchenko <shaurmanaaviamotornoy@gmail.com>
2022-02-22 00:44:52 +0300

    Переходы между страницами

---
+++ pages/login_page.py
@@ -0,0 +1,20 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert 'login' in self.browser.current_url, "Ссылка на страницу логина неверная"
+
+    def should_be_login_form(self):
+        assert self.browser.find_element(*LoginPageLocators.LOGIN_FORM), "На странице регистрации и логина нет формы " \
+                                                                         "логина "
+
+    def should_be_register_form(self):
+        assert self.browser.find_element(*LoginPageLocators.REGISTRATION_FORM,
+                                         'На странице регистрации и логина нет формы регистрации')
